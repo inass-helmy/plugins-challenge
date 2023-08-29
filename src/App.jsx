4a2614c4b63cccd54946cc59ab12b8e6df762d95
Enas Samir <inass.helmy@gmail.com>
2023-08-29 22:02:55 +0300

    create the main page layout

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation/index";
+import Marketing from "./Containers/Marketing/Marketing";
+import Finance from "./Containers/Finance/Finance";
+import Personnel from "./Containers/Personnel/Personnel";
+import { useState } from "react";
+
+function App() {
+  const [isAllPluginsDisabled, setIsPluginsDisabled] = useState(false);
+
+  const handleToggleAllPlugins = () => {
+    setIsPluginsDisabled(!isAllPluginsDisabled);
+  };
+
+  return (
+    <div className='app-container'>
+      <Router>
+        <Navigation onDisableHandler={handleToggleAllPlugins} />
+        <Routes>
+          <Route path='/' element={<Marketing isAllPluginsDisabled={isAllPluginsDisabled} />} />
+          <Route
+            path='/marketing'
+            element={<Marketing isAllPluginsDisabled={isAllPluginsDisabled} />}
+          />
+          <Route
+            path='/finance'
+            element={<Finance isAllPluginsDisabled={isAllPluginsDisabled} />}
+          />
+          <Route
+            path='/personnel'
+            element={<Personnel isAllPluginsDisabled={isAllPluginsDisabled} />}
+          />
+          <Route path='*' element={<h2>Page not found</h2>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
